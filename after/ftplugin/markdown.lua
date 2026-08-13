2e83874b8c6c22e4777dd195e6ac81379d3a0767
Tom Shafer <tom@tshafer.com>
2026-08-13 08:19:27 -0400

    Fix markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
 -- Markdown settings
 -- Auto-wrap at 72 chars
 vim.opt_local.textwidth = 72
-vim.opt_local.formatoptions = vim.opt_local.formatoptions .. 't'
+vim.opt_local.formatoptions:append 't'
