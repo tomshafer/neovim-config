78de18499c20f46f425a5065b48672b528059648
Tom Shafer <tom@tshafer.com>
2026-08-13 08:20:45 -0400

    Make Lua setups faster

--- init.lua
+++ init.lua
@@ -725,10 +725,8 @@ do
           },
           workspace = {
             checkThirdParty = false,
-            library = vim.tbl_filter(
-              function(d) return not d:match(vim.fn.stdpath 'config' .. '/?a?f?t?e?r?') end,
-              vim.api.nvim_get_runtime_file('', true)
-            ),
+            -- From GPT 5.6
+            library = { vim.env.VIMRUNTIME, vim.api.nvim_get_runtime_file('lua/lspconfig', false)[1] },
           },
         })
       end,
