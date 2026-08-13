bd1ec0516cf310aafa980c7da85742223c7b25b2
Tom Shafer <tom@tshafer.com>
2026-08-12 21:44:30 -0400

    Make Lua loads faster

--- init.lua
+++ init.lua
@@ -725,9 +725,10 @@ do
           },
           workspace = {
             checkThirdParty = false,
-            -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-            --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-            library = vim.api.nvim_get_runtime_file('', true),
+            library = vim.tbl_filter(
+              function(d) return not d:match(vim.fn.stdpath 'config' .. '/?a?f?t?e?r?') end,
+              vim.api.nvim_get_runtime_file('', true)
+            ),
           },
         })
       end,
